3309ddd4764e448bfa3c57cae1e54ffc186153a4
Vanessa Rivera <vanessa.rivera@sjsu.edu>
2020-03-15 16:43:30 -0700

    set up basic drawing machine
    1) how to save canvas an image
    2) how to display
    3) how to draw by pulling from array

--- sketch.js
+++ sketch.js
@@ -1,7 +1,42 @@
+let array = [];
+
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(255);
+  strokeWeight(5);
+  noFill();
 }
 
 function draw() {
-  background(220);
+  if (mouseIsPressed){
+    stroke(map(mouseX, 0, 600, 0, 255, true));
+    //line(mouseX, mouseY, pmouseX, pmouseY);
+    background(0);
+    array.push([mouseX, mouseY]);
+  }
+
+
+}
+
+
+function keyTyped(){
+  if (key === 's'){
+    //save this image
+    saveCanvas('fileName', 'png');
+  }else if (key === 'd'){
+    background(255);
+
+    //display image
+    beginShape();
+    for (let i = 0; i < array.length-1; i++) {
+      //line(array[i][0], array[i][1], array[i + 1][0], array[i + 1][1]);
+      curveVertex(array[i][0], array[i][1])
+    }
+    endShape();
+
+
+  }
+
+  return false;
 }
