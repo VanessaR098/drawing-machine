b5369d9447e0ecf3b252372cb0c3f62d325e0777
Vanessa Rivera <vanessa.rivera@sjsu.edu>
2020-03-16 17:51:27 -0700

    add details: dif colored pens, background title
    press corresponding letters to change color

--- sketch.js
+++ sketch.js
@@ -1,5 +1,7 @@
 let array = [];
-
+let colorR = 0;
+let colorG = 0;
+let colorB = 0;
 
 function setup() {
   createCanvas(600, 600);
@@ -8,14 +10,48 @@ function setup() {
   noFill();
 }
 
+// function preload(){
+//
+//
+// }
+
 function draw() {
+
   if (mouseIsPressed){
-    stroke(map(mouseX, 0, 600, 0, 255, true));
-    //line(mouseX, mouseY, pmouseX, pmouseY);
-    background(0);
+    stroke(colorR, colorG, colorB);//map(mouseX, 0, 600, 0, 255, true));
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    //background(0);
     array.push([mouseX, mouseY]);
   }
 
+
+
+
+}
+
+function keyPressed(){
+  if (key === 'r'){
+    colorR = 255;
+    colorG = 77;
+    colorB = 77;
+  }else if(key === 'g'){
+    colorR = 0;
+    colorG = 255;
+    colorB = 0;
+  }else if(key === 'b'){
+    colorR = 77;
+    colorG = 136;
+    colorB = 255;
+  }else if(key === 'y'){
+    colorR = 255;
+    colorG = 255;
+    colorB = 102;
+  }else if(key === 'k'){
+    colorR = 0;
+    colorG = 0;
+    colorB = 0;
+  }
+  return false;
 
 }
 
@@ -37,6 +73,8 @@ function keyTyped(){
 
 
   }
+
+
 
   return false;
 }
